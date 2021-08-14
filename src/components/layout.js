3cc4dbb2bfd8bbbf3d9fce93a3dc6c06461afddd
mathew461 <mathew461@gmail.com>
2021-08-14 13:44:31 +0100

    [Original Commit]

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,22 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import styled, { ThemeProvider } from 'styled-components'
+import { theme } from '../utils/theme'
 
 import Header from "./header"
 import "./layout.css"
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
+const ChildContainer = styled.div`
+  flex-grow: 1;
+  margin-top: 97px;
+`
+
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,16 +30,10 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
+    <ThemeProvider theme={theme}>
+      <PageContainer>  
+        <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+        <ChildContainer>{children}</ChildContainer>
         <footer
           style={{
             marginTop: `2rem`,
@@ -43,8 +43,8 @@ const Layout = ({ children }) => {
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
-      </div>
-    </>
+      </PageContainer>
+    </ThemeProvider>
   )
 }
 
